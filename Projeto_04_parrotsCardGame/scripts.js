007d4da76530c98cfaa9fd08623505a7e6fc5d7f
Diogo <diogolberaldo@gmail.com>
2022-04-08 17:05:03 -0300

    Layout quase pronto

--- Projeto_04_parrotsCardGame/scripts.js
+++ Projeto_04_parrotsCardGame/scripts.js
@@ -1,7 +1,12 @@
+const memes = ["./Imagens/bobrossparrot.gif", "./Imagens/explodyparrot.gif", "./Imagens/fiestaparrot.gif", "./Imagens/metalparrot.gif", "./Imagens/revertitparrot.gif", "./Imagens/tripletsparrot.gif", "./Imagens/unicornparrot.gif"];
+
 let numCartas = prompt("Com quantas cartas você quer jogar?");
 
 if(numCartas < 4 || numCartas%2!==0 || numCartas > 14){
     repetirNumCartas();
+}
+else{
+    montarBaralho();
 }
 
 function repetirNumCartas(){
@@ -9,5 +14,29 @@ function repetirNumCartas(){
     while(numCartas < 4 || numCartas%2 !==0 || numCartas > 14)
 }
 
+memes.sort(comparador);
 
 
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+function montarBaralho() {
+    const baralho = document.querySelector(".card-space");
+    baralho.innerHTML = "";
+    console.log(memes)
+    for (let i = 0; i < numCartas; i++) {
+        baralho.innerHTML += `
+        <div class="card" onclick="virarCarta(this)">
+        <img class="frente" src="./Imagens/front.png"/>
+        <img class="verso escondido" src=""/>
+        </div>
+        `;
+    }
+}
+
+function virarCarta(elemento){
+
+
+}
